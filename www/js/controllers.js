eda0cf3751fcc73a04e229dbec1481bb87db93c3
Erica Lloyd <email": null>
2016-05-14 14:01:19 -0500

    add home page template, register/login buttons, placeholder img

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -34,6 +34,29 @@ angular.module('starter')
   };
 })//
 
+.controller('homeCtrl', function($scope) {
+  $scope.options = {
+    loop: false,
+    effect: 'fade',
+    speed: 500,
+  }
+
+  $scope.$on("$ionicSlides.sliderInitialized", function(event, data){
+    // data.slider is the instance of Swiper
+    $scope.slider = data.slider;
+  });
+
+  $scope.$on("$ionicSlides.slideChangeStart", function(event, data){
+    console.log('Slide change is beginning');
+  });
+
+  $scope.$on("$ionicSlides.slideChangeEnd", function(event, data){
+    // note: the indexes are 0-based
+    $scope.activeIndex = data.activeIndex;
+    $scope.previousIndex = data.previousIndex;
+  });
+})
+
 .controller('picksCtrl', function($scope) {
 
 })//
--- www/js/routes.js
+++ www/js/routes.js
@@ -2,30 +2,35 @@ angular.module('starter', ['ionic', 'ionic.contrib.ui.tinderCards'])
 .config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
 
-    .state('app', {
+  .state('app', {
     url: '/app',
     abstract: true,
     templateUrl: 'templates/sideMenu.html',
     controller: 'AppCtrl'
   })
   .state('app.start-picking', {
-      url: '/start-picking',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/startPicking.html',
-          controller: 'profileCtrl'
-        }
+    url: '/start-picking',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/startPicking.html',
+        controller: 'profileCtrl'
       }
-    })
+    }
+  })
   .state('app.edit-profile', {
-        url: '/edit-profile',
-        views: {
-          'menuContent': {
-            templateUrl: 'templates/editProfile.html',
-            controller: 'editProfileCtrl'
-          }
-        }
-      });
+    url: '/edit-profile',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/editProfile.html',
+        controller: 'editProfileCtrl'
+      }
+    }
+  })
+  .state('home', {
+    url: '/home',
+    templateUrl: 'templates/home.html',
+    controller: 'homeCtrl'
+  });
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/start-picking');
+  $urlRouterProvider.otherwise('/home');
 });
