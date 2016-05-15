30a6944691d54c142c03640aab90997982b1a772
Erica Lloyd <email": null>
2016-05-15 11:24:52 -0500

    Add  module, factory example

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.routes', 'starter.services', 'starter.directives', 'ionic.contrib.ui.tinderCards'])
+angular.module('starter', ['ionic', 'starter.controllers','starter.services', 'starter.directives', 'ionic.contrib.ui.tinderCards', 'ngResource'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -20,4 +20,116 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.routes', 'st
       StatusBar.styleDefault();
     }
   });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+  // home page and register
+  .state('home', {
+    url: '/home',
+    templateUrl: 'templates/home.html',
+    controller: 'homeCtrl'
+  })
+  .state('register', {
+    url: '/register',
+    templateUrl: 'templates/register.html',
+    controller: 'registerCtrl'
+  })
+
+  // start of app after login/register
+  .state('app', {
+    url: '/app',
+    abstract: true,
+    templateUrl: 'templates/sideMenu.html',
+    controller: 'AppCtrl'
+  })
+
+  // profile page
+  .state('app.profile', {
+    // placeholder url until we know for sure
+    // url: '/profile/:userId',
+    url: '/profile',
+    views: {
+        'menuContent': {
+          templateUrl: 'templates/profile.html',
+          controller: 'profileCtrl'
+        }
+      }
+  })
+
+  // edit profile page
+  .state('app.edit-profile', {
+    // url: '/profile/:userId/edit',
+    url: '/edit-profile',
+    views: {
+        'menuContent': {
+          templateUrl: 'templates/editProfile.html',
+          controller: 'editProfileCtrl'
+        }
+    }
+  })
+
+  // edit roles on profile
+  .state('app.edit-my-roles', {
+    // url: '/profile/:userId/edit/my-roles',
+    url: '/edit-my-roles',
+    views: {
+        'menuContent': {
+          templateUrl: 'templates/editMyRoles.html',
+          controller: 'editMyRolesCtrl'
+        }
+      }
+  })
+
+  // edit most frequent roles searched
+  .state('app.edit-search-roles', {
+    // url: '/profile/:userId/edit/search-roles',
+    url: '/edit-search-roles',
+    views: {
+        'menuContent': {
+          templateUrl: 'templates/editSearchRoles.html',
+          controller: 'editSearchRolesCtrl'
+        }
+      }
+  })
+
+  // start picking with current user
+  .state('app.start-picking', {
+    // url: '/profile/:userId/start-picking',
+    url: '/start-picking',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/startPicking.html',
+        controller: 'startPickingCtrl'
+      }
+    }
+  })
+
+  // see all picks on your account
+  .state('app.picks', {
+    // url: '/profile/:userId/picks',
+    url: '/picks',
+    views: {
+        'menuContent': {
+          templateUrl: 'templates/picks.html',
+          controller: 'picksCtrl'
+        }
+      }
+  })
+
+  // app settings page
+  // .state('app.settings', {
+  //   url: '/settings',
+  //   views: {
+  //     'menuContent': {
+  //       templateUrl: 'views/settings/settings.html',
+  //       controller: 'settingsCtrl'
+  //     }
+  //   }
+  // })
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/home');
+
 });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,4 +1,4 @@
-angular.module('starter')
+angular.module('starter', [])
 .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
 
   // With the new view caching in Ionic, Controllers are only called
@@ -69,6 +69,7 @@ angular.module('starter')
 
 })//
 
+
 .controller('editMyRolesCtrl', function($scope) {
 
 })//
@@ -85,8 +86,14 @@ angular.module('starter')
 
 })//
 
-.controller('rolesCtrl', function($scope) {
-
+//factory example using resources
+.controller('rolesCtrl', function($scope, Role) {
+  //get all roles
+  $scope.roles = Role.query();
+  console.log($scope.roles);
+  //get one roll
+  $scope.role = Role.query({role: 1});
+  console.log($scope.role);
 })//
 
 //Cards Controller - Start Picking
@@ -141,9 +148,5 @@ angular.module('starter')
     console.log('RIGHT SWIPE');
     $scope.addCard();
   };
-})//
+});//
 
-.controller('editProfileCtrl', function($scope, $ionicModal, $timeout) {
-
-
-});
