2a3c33adbfb73d1a4d2e4c159c33558e9d3f6116
wesleye2003 <el-amin.7@buckeyemail.osu.edu>
2016-05-16 15:33:59 -0500

    add android platform

---
+++ platforms/android/assets/www/js/services.js
@@ -0,0 +1,34 @@
+angular.module('starter.services',['ngResource'])
+//factory example using resources
+.factory('User', function($resource, $http) {
+   return $resource('http://floating-tor-67033.herokuapp.com/users/:id', {id: "@id"});
+})
+
+//send post to login route, which will return a user object
+.factory('LoggedInUser', function($resource, $http) {
+   return $resource('http://floating-tor-67033.herokuapp.com/login/:id', {id: "@id"});
+})
+
+.factory('Pick', function($resource, $http) {
+   return $resource('http://floating-tor-67033.herokuapp.com/users/:id/pickings', {id: "@id"});
+})
+
+.factory('Role', function($resource, $http) {
+    return $resource('http://floating-tor-67033.herokuapp.com/roles/:id', {id: "@id"});
+})
+
+.factory('Genre', function($resource, $http){
+  return $resource('http://floating-tor-67033.herokuapp.com/genres/:id', {id: "@id"});
+})
+
+.factory('ArtistRole', function($resource, $http) {
+  return $resource('http://floating-tor-67033.herokuapp.com/users/:id/roles', {id: "@id"});
+})
+
+.factory('SearchedRole', function($resource, $http) {
+  return $resource('http://floating-tor-67033.herokuapp.com/users/:id/searched_roles', {id: "@id"});
+})
+
+
+.service('BlankService', [function(){
+}]);
