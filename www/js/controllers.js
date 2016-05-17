17439b9768badad7e3b14368dfcc1e4beb6b3897
Erica Lloyd <email": null>
2016-05-17 12:32:31 -0500

    Profile page styling

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -34,12 +34,12 @@ angular.module('starter.controllers',[])
 
   // Perform the login action when the user submits the login form
   $scope.doRegister = function(form){
-    var data = { zip: form.zip.$modelValue, username: form.username.$modelValue, password: form.password.$modelValue}
+    var data = { zipcode: form.zip.$modelValue, username: form.username.$modelValue, password: form.password.$modelValue}
     console.log(data);
 
     $http({url:"http://floating-tor-67033.herokuapp.com/users",
            method: 'POST',
-           data: { zip: form.zip.$modelValue, username: form.username.$modelValue, password: form.password.$modelValue}}).success(function(response){
+           data: { zipcode: form.zip.$modelValue, username: form.username.$modelValue, password: form.password.$modelValue}}).success(function(response){
       window.localStorage['id'] = response.id;
       $state.go('app.profile');
       $scope.closeRegister();
